7038ec5e16a63481ef2ecc5b87d957f381e4db5b
daniel-wells <daniel-wells@users.noreply.github.com>
2017-06-02 16:47:41 +0100

    add dimnames back into results
    SDA doesn't accept column or row names so these have to be re added
    after running

--- R/load_results.R
+++ R/load_results.R
@@ -7,6 +7,8 @@
 #' 
 #' @param iteration integer; iteration number of save to load
 #' 
+#' @param data_path string; path to location data was saved (using export_data). Used to reload gene and individual names
+#' 
 #' @return A list of matrices
 #'
 #' @examples
@@ -15,7 +17,7 @@
 #' @export
 #' @import data.table
 
-load_results <- function(results_folder, iteration) {
+load_results <- function(results_folder, iteration, data_path=NULL) {
   
   folder <- paste0(results_folder, "/it", iteration)
   stopifnot(dir.exists(folder))
@@ -35,11 +37,15 @@ load_results <- function(results_folder, iteration) {
   out1$free_energy <- out$free_energy
   out1$miss <- out$miss
   
+  
   # if log saved, extract PIP fraction < 0.5
   if (file.exists(paste0(results_folder, "/log.txt"))){
     out1$pip_fraction <- fread(paste0("grep -o '[0]) : [0-9].*[0-9]*' ", results_folder, "/log.txt", sep=" "))[1:iteration]$V3
+  } else {
+    message(paste0("Log file containing pip fraction per iteration not found: '", results_folder, "/log.txt'"))
   }
 
+  
   # if command.txt saved, extract arguments used
   if (file.exists(paste0(results_folder, "/command.txt"))){
     command <- readLines(paste0(results_folder, "/command.txt"), warn=F)
@@ -51,6 +57,31 @@ load_results <- function(results_folder, iteration) {
     names(values) <- unlist(command)[c(TRUE,FALSE)]
     
     out1$command_arguments <- as.data.table(t(values))
+  } else {
+    message(paste0("Command file containing SDA run parameters not found: '", results_folder, "/command.txt'"))
+  }
+  
+  
+  # if dimnames saved, add back in
+  if (is.null(data_path)){
+    dimnames_file <- gsub(".data","_dimnames.rds",out1$command_arguments$data)
+  }else{
+    dimnames_file <- paste0(data_path, basename(gsub(".data","_dimnames.rds",out1$command_arguments$data)))
+  }
+  
+  if (file.exists(dimnames_file)){
+    original_dimnames <- readRDS(dimnames_file)
+    
+    # add individual names to scores
+    rownames(out1$scores) <- original_dimnames[[1]]
+    
+    # add gene names to loadings & pips
+    for (i in length(out1$loadings)){
+      colnames(out1$loadings[[i]]) <- original_dimnames[[2]]
+      colnames(out1$pips[[i]]) <- original_dimnames[[2]]
+    }
+  } else {
+    message(paste0("File containing gene and individual lables not found: '", dimnames_file,"'"))
   }
   
   return(out1)
